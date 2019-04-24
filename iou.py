b8619e380622da855537b88f2915fae71005ad10
laizp <1550377556@qq.com>
2019-04-24 18:35:40 +0800

    modify iou func

--- iou.py
+++ iou.py
@@ -1,17 +1,18 @@
 #usr/bin/python
 
 # Intersection over Union func
-def IoU(x1,y1,w1,h1,x2,y2,w2,h2):
+def IoU(r1,r2):
     """calculate iou between 2 rectangles
     
     Arguments:
-        x1 y1 w1 h1 {int/float} -- topleft point (x1,y1), width(w1) and height(h1)
-        x2 y2 w2 h2 {int/float} -- topleft point (x2,y2), width(w2) and height(h2)
+        r1 {tuple or list} -- first rectangle containing topleft point (x1,y1), width(w1) and height(h1)
+        r2 {tuple or list} -- second rectangle containing topleft point (x2,y2), width(w2) and height(h2)
     
     Returns:
         {float} -- iou result
     """
-    
+    x1,y1,w1,h1=r1
+    x2,y2,w2,h2=r2
     assert x1>0 and y1>0 and w1>0 and h1>0 and x2>0 and y2>0 and w2>0 and h2>0
 
     interw=min(x1+w1,x2+w2)-max(x1,x2)
